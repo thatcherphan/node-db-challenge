ca1a553b610f14c910c92b63f393ded2e5e11c34
Th5tch3r <amber.phan27@gmail.com>
2020-02-07 23:28:17 -0500

    get and add project endpoint done

---
+++ routes/projectModel.js
@@ -0,0 +1,28 @@
+const db = require('../data/db-config');
+
+module.exports = {
+    getProjects,
+    getProjectById,
+    addProject,
+    // getResources,
+    // getResourcesById,
+    // addResources,
+    // getTasks,
+    // addTask
+}
+
+function getProjects() {
+    return db('projects')
+}
+
+function getProjectById(id) {
+    return db('projects')
+      .where({id})
+      .first()
+  }
+
+  function addProject(project) {
+    return db('projects')
+        .insert(project, 'id')
+        .then(([id]) => getProjectById(id));
+}
--- routes/projectsRouter.js
+++ routes/projectsRouter.js
@@ -1,11 +1,49 @@
 const express = require('express');
 
-const db = require('./projectModel');
+const Projects = require('./projectModel');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).json({message: "alive"})
+    Projects.getProjects()
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({message: "Error retrieving projects"})
+        })
+})
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+
+    Projects.getProjectById(id)
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({message: "Error getting this project with this id"})
+        })
+})
+
+router.post('/', (req, res) => {
+    const data = req.body;
+
+    Projects.addProject(data)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({message: "Error adding this project"})
+        })
+        // {
+        //     "name": "Go buy phone",
+        //     "description": "256G",
+        //     "completed": false
+        // }
 })
 
 module.exports = router;
